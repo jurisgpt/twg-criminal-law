6f4e8c45f2416d56acc43eb146c4790ba357588e
Nouman Arshad <noumanarshad15926@gmail.com>
2023-11-26 18:34:48 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
 
     <main>
       <Header />
-      {/* <Hero /> */}
-      <Account />
+      <Hero />
+      {/* <Account /> */}
       <Footer />
     </main>
   )
