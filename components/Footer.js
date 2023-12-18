f9b6b5e775e8ab4818424728d072e5ba21bd0c5f
Nouman Arshad <noumanarshad15926@gmail.com>
2023-12-18 20:55:06 +0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,6 +6,15 @@ import logo from "@/app/icon.png";
 import LinkedinLogo from '@/assets/icons/linkden.png'
 import TwitterLogo from '@/assets/icons/twitter.png'
 
+const tristian = {
+    address: `Tristan W. Gillespie, Esq.
+    11539 Park Woods Cir, Suite 304, Alpharetta, GA 30005`,
+    gmail: 'abc@gmail.com',
+    phoneNumber: '404-276-7277',
+}
+
+const contact = <div className="btn btn-primary max-sm:mt-4 max-sm:mx-2 bg-[#ffffff] p-4 text-black cursor-pointer hover:bg-slate-200">Call Now : {tristian.phoneNumber}</div>
+
 const Footer = () => {
     return (
         <footer className="bg-black text-white max-sm:mt-24 border-t border-base-content/10">
@@ -31,8 +40,7 @@ const Footer = () => {
                         </Link>
 
                         <p className="mt-3 text-sm text-base-content/80">
-                            Tristan W. Gillespie, Esq.
-                            11539 Park Woods Cir, Suite 304, Alpharetta, GA 30005
+                            {tristian.address}
                         </p>
                     </div>
                     <div className="flex-grow flex flex-wrap justify-center -mb-10 md:mt-0 mt-10 text-center">
@@ -43,10 +51,10 @@ const Footer = () => {
 
                             <div className="flex flex-col justify-center items-center md:items-start gap-2 mb-10 text-sm">
                                 <Link href="/#contactus" className="link link-hover">
-                                    404-276-7277
+                                    {tristian.phoneNumber}
                                 </Link>
                                 <Link href="/#email" className="link link-hover">
-                                    abc@gmail.com
+                                    {tristian.gmail}
                                 </Link>
 
                                 <div className="flex flex-row space-x-2">
@@ -55,7 +63,7 @@ const Footer = () => {
                                         target="_blank" className="link link-hover">
                                         <Image src={LinkedinLogo}
                                             class="dark:!invert hover:opacity-60 transition-opacity duration-500 active:opacity-80"
-                                            alt="linkedin"
+                                            alt="linkedin profile"
                                             width={40}
                                             height={40}
                                         />
@@ -63,7 +71,7 @@ const Footer = () => {
                                     <Link href="/#twitter" className="link link-hover">
                                         <Image src={TwitterLogo}
                                             class="dark:!invert hover:opacity-60 transition-opacity duration-500 active:opacity-80"
-                                            alt="linkedin"
+                                            alt="twitter profile"
                                             width={40}
                                             height={40}
                                         />
@@ -72,7 +80,8 @@ const Footer = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="btn btn-primary max-sm:mt-4 max-sm:mx-2 bg-[#ffffff] p-4 text-black cursor-pointer hover:bg-slate-200">Call Now : 404-276-7277</div>
+                    {/* Contact Info */}
+                    {contact}
                 </div>
             </div>
         </footer>
