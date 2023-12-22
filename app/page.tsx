9c72fbd7bc8d4bca22fe53c4500e5ec44ff377fe
Nouman Arshad <noumanarshad15926@gmail.com>
2023-12-22 11:59:32 +0500

    Analytics Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header"
 import Hero from '@/components/Hero'
 import Footer from '@/components/Footer'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Footer />
+      <Analytics />
     </main>
   )
 }
