4532af5bf13440914b47864457b9a94de5a00f39
Nouman Arshad <noumanarshad15926@gmail.com>
2024-01-14 18:39:10 +0500

    FAQ/Reviews

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Header from "@/components/Header"
 import Hero from '@/components/Hero'
 import Footer from '@/components/Footer'
+import FAQ from '@/components/FAQ'
+import Testimonial3 from '@/components/Testimonials3'
 import { Analytics } from '@vercel/analytics/react'
 
 export default function Home() {
@@ -8,6 +10,8 @@ export default function Home() {
     <main>
       <Header />
       <Hero />
+      <Testimonial3 />
+      <FAQ />
       <Footer />
       <Analytics />
     </main>
